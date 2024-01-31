60fdf38b8c94c98fbfa3b401035fe5f7906bc81d
Mathias L. Baumann <mathias.baumann@frequenz.com>
2024-01-31 17:20:16 +0100

    Remove cookiecutter todos
    Signed-off-by: Mathias L. Baumann <mathias.baumann@frequenz.com>

--- tests/test_dispatch_client.py
+++ tests/test_dispatch_client.py
@@ -3,16 +3,3 @@
 
 """Tests for the frequenz.client.dispatch package."""
 import pytest
-
-from frequenz.client.dispatch import delete_me
-
-
-def test_dispatch_client_succeeds() -> None:  # TODO(cookiecutter): Remove
-    """Test that the delete_me function succeeds."""
-    assert delete_me() is True
-
-
-def test_dispatch_client_fails() -> None:  # TODO(cookiecutter): Remove
-    """Test that the delete_me function fails."""
-    with pytest.raises(RuntimeError, match="This function should be removed!"):
-        delete_me(blow_up=True)
